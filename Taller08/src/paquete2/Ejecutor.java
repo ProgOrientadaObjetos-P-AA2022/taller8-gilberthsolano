4d44bfca27ef56351f2eefe447ca4d81f326a5f4
gilbertsolano <gilberthsolanosotomayor@gmail.com>
2022-06-14 23:04:02 -0500

    cambios

--- Taller08/src/paquete2/Ejecutor.java
+++ Taller08/src/paquete2/Ejecutor.java
@@ -19,40 +19,46 @@ public class Ejecutor {
         Scanner entrada = new Scanner(System.in);
         DocenteNombramiento dn1 = new DocenteNombramiento();
         DocenteFactura df1 = new DocenteFactura();
-        System.out.println("----Docuente Nombramiento---");
-        System.out.println("Ingrese el nombre del docente");
-        dn1.establecerNombres(entrada.nextLine());
-        System.out.println("Ingrese el apellido del docente");
-        dn1.establecerApellidos(entrada.nextLine());
-        System.out.println("Ingrese la identificacion del docente");
-        dn1.establecerIdentificacion(entrada.nextLine());
-        System.out.println("Ingrese la edad del docente");
-        dn1.establecerEdad(Integer.parseInt(entrada.nextLine()));
-        System.out.println("Ingrese el sueldo del docente");
-        dn1.establecerValorSueldo(Double.parseDouble(entrada.nextLine()));
-        System.out.println("Ingrese el valor de hora extra");
-        dn1.establecerValorHoraExtra(Double.parseDouble(entrada.nextLine()));
-        System.out.println("Ingrese el numero de horas extras");
-        dn1.establecerNumeroHorasExtras(Double.parseDouble(entrada.nextLine()));
-        dn1.calcularSueldoFinal();
+        int opc = 0;
+        System.out.println("Menu");
+        System.out.println("1. Docente Nombramiento");
+        System.out.println("2. Docente Factura");
+        opc = Integer.parseInt(entrada.nextLine());
+        if (opc > 2 || opc < 1) {
+            System.out.println("Error opcion no valida");
 
-        System.out.printf("%s", dn1);
-        System.out.println("---Docente Factura---");
-        System.out.println("Ingrese el nombre");
-        df1.establecerNombres(entrada.nextLine());
-        System.out.println("Ingrese el apellido");
-        df1.establecerApellidos(entrada.nextLine());
-        System.out.println("Ingrese identificacion");
-        df1.establecerIdentificacion(entrada.nextLine());
-        System.out.println("Ingrese la edad");
-        df1.establecerEdad(Integer.parseInt(entrada.nextLine()));
-        System.out.println("Ingrese el valor de la factura");
-        df1.establecerValorFactura(Double.parseDouble(entrada.nextLine()));
-        System.out.println("Ingrese valor iva descuento");
-        df1.establecerValorIvaDescuento(Double.parseDouble(entrada.nextLine()));
-        df1.calcularValorCancelar();
-        System.out.println("-----------------");
-        System.out.printf("%s", df1);
+        } else {
+            System.out.println("Ingrese el nombre del docente");
+            dn1.establecerNombres(entrada.nextLine());
+            System.out.println("Ingrese el apellido del docente");
+            dn1.establecerApellidos(entrada.nextLine());
+            System.out.println("Ingrese la identificacion del docente");
+            dn1.establecerIdentificacion(entrada.nextLine());
+            System.out.println("Ingrese la edad del docente");
+            dn1.establecerEdad(Integer.parseInt(entrada.nextLine()));
+            if (opc == 1) {
+                System.out.println("----Docuente Nombramiento---");
+                System.out.println("Ingrese el sueldo del docente");
+                dn1.establecerValorSueldo(Double.parseDouble(entrada.nextLine()));
+                System.out.println("Ingrese el valor de hora extra");
+                dn1.establecerValorHoraExtra(Double.parseDouble(entrada.nextLine()));
+                System.out.println("Ingrese el numero de horas extras");
+                dn1.establecerNumeroHorasExtras(Double.parseDouble(entrada.nextLine()));
+                dn1.calcularSueldoFinal();
+
+                System.out.printf("%s", dn1);
+            }
+            if (opc == 2) {
+                System.out.println("---Docente Factura---");
+                System.out.println("Ingrese el valor de la factura");
+                df1.establecerValorFactura(Double.parseDouble(entrada.nextLine()));
+                System.out.println("Ingrese valor iva descuento");
+                df1.establecerValorIvaDescuento(Double.parseDouble(entrada.nextLine()));
+                df1.calcularValorCancelar();
+                System.out.println("-----------------");
+                System.out.printf("%s", df1);
+            }
+        }
 
     }
 }
